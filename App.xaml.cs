981613e0cf6371d8dbb7283b774fb08e0792e229
Dubby27 <efarbicka@gmail.com>
2025-09-23 15:23:01 +0200

    Flyout

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Hexagon
+﻿using System.ComponentModel;
+
+namespace Hexagon
 {
     public partial class App : Application
     {
@@ -9,7 +11,10 @@
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            AppShell shell = new AppShell();
+            Window window = new Window(shell);
+
+            return window;
         }
     }
 }
--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,7 +1,11 @@
-﻿namespace Hexagon
+﻿using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.PlatformConfiguration.iOSSpecific;
+
+namespace Hexagon
 {
     public partial class AppShell : Shell
     {
+
         public AppShell()
         {
             InitializeComponent();
