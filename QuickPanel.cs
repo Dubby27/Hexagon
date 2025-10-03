8089021cc3a0a9c27af068be0ca14b90698e6cdb
Dubby27 <81176253+Dubby27@users.noreply.github.com>
2025-10-03 11:30:02 +0200

    Quick Panel Class & Struct

---
+++ QuickPanel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hexagon
+{
+    public struct QuickPanelStruct
+    {
+        public string? upper;
+        public string? title;
+        public string? lower;
+        public HorizontalStackLayout? timetable;
+    }
+
+    internal static class QuickPanel
+    {
+
+    }
+}
